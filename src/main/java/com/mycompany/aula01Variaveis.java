c3c968ce9dc1a52527a211e50112ef79b47b8eaa
FRANCESCON777 <joao.francescon@escola.pr.gov.br>
2025-03-26 13:53:59 +0000

    Aula 01 de variáveis

---
+++ src/main/java/com/mycompany/aula01Variaveis.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+public class aula01Variaveis {
+
+    public static void main (String[] args) }
+    // Variavel lógica 
+
+    // tipo primitivo 
+    boolean Valorlogicoprimativo = true;
+
+    //Tipo abstrato 
+    boolean Valorlogicoabstrato = false;
+
+}
